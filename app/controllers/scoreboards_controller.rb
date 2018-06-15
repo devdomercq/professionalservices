3a6a651bb8241bf5c71917e9f5fbd92091a46d61
Devon Domercq <djdomercq@gmail.com>
2018-06-15 18:26:05 +0000

    Add Scoreboards to App Index and Show Actions and View Pages

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,20 +1,33 @@
 class ScoreboardsController < ApplicationController
-    
+    before_action :find_scoreboard, only: [:show, :edit, :update, :destroy]
     def index
+        @scoreboards = Scoreboard.all.order("created_at DESC")
     end
-    
+    def show
+       
+    end
     def new
         @scoreboard = Scoreboard.new
     end
     
     def create
-        @scoreboard = Scoreboard.new(play_params)
+        @scoreboard = Scoreboard.new(scoreboard_params)
+        
+        if @scoreboard.save 
+            redirect_to root_path
+        else
+           render 'new'
+        end
     end
     
     private
     
     def scoreboard_params
-        params.require(:scoreboard).permit(:bpecode, :organization, :code, :description, :eventdate)
+        params.require(:scoreboard).permit(:eventdate, :bpecode, :organization, :code, :description)
+    end
+    
+    def find_scoreboard
+         @scoreboard = Scoreboard.find(params[:id])
     end
 end
 
